b20f9a95df52a4867cbd14a567c9aed82d3c7091
Mathias Roux <mathiasroux.pro@gmail.com>
2022-04-23 16:07:59 +0200

    loading screen done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React, { useState, useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import LoadingScreen from './components/LoadingScreen'
 
 function App() {
+  const [loading, setLoadding] = useState(true)
+
+  useEffect (() => {
+    setTimeout(() => setLoadding(false), 6000)
+  }, [])
+
   return (
-    <div className="App">
-      <div className="Home">
-          <span>HI THERE!</span>
+    <>
+    {loading === false ? (
+      <div className="App">
+        <div className="Home">
+          <h1 className="title">HI THERE!</h1>
+          <p className="details">
+            First <i>loading screen</i> made with React & Css
+          </p>
+        </div>
       </div>
-    </div>
+    ) : (
+          <LoadingScreen />
+    )}
+    </>
   );
 }
 
---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LoadingScreen.css'
+
+const LoadingScreen = () => {
+	return (
+		<div className="loading-wrapper">
+			<div className="loading-text">
+				<span>Page is loading</span>
+			</div>
+		</div>
+	)
+}
+
+
+export default LoadingScreen
